d67f841f745d37f7e928a5f3c12c93610d726fd7
ir-ir-ir <kira0_05@mail.ru>
2024-12-07 15:37:25 +0300

    hw4 is added

---
+++ Internet_hw4/src/Main.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+    static void connection(String str_url) {
+
+        System.out.println();
+        System.out.println("Response: ");
+
+        try {
+            // создание url
+            URL url = new URL(str_url);
+            // подключение к ресурсу
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+
+            // получение содержимого ресурса
+            try(InputStream os = url.openStream();                 // входной поток байт
+                InputStreamReader is = new InputStreamReader(os); // входной поток символов
+                BufferedReader br = new BufferedReader(is))       // буфферезированный поток символов
+            {
+                // печать
+                String str;
+                while ((str = br.readLine()) != null){
+                    if ((str.trim()).equals("},")){ continue; }
+                    int f = 0; // индитификатор для печати переноса строки
+                    for (int i = 0; i < str.length(); i++){
+                        char a = str.charAt(i);
+                        if ((a !=  '[') && (a !=  ']') && (a !=  '{') && (a !=  '}') && (a !=  '"')){
+                            System.out.print(a);
+                            f = 1;
+                        }
+                    }
+                    if (f == 1) {System.out.println();}
+                }
+            }
+            catch (IOException e){
+                System.out.println("Не удалось установить соединение с ресурсом!");
+            }
+
+            // разрыв соединения
+            conn.disconnect();
+        }
+        catch (IOException e) {
+            System.out.println("Не удалось установить соединение с ресурсом!");
+        }
+
+        System.out.println("End of the response");
+    }
+
+    public static void main(String[] args) {
+
+        String url1 = "https://fake-json-api.mock.beeceptor.com/users";
+        String url2 = "https://dummy-json.mock.beeceptor.com/todos";
+        connection(url1);
+        connection(url2);
+
+    }
+}
